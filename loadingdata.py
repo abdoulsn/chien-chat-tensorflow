88bfe47654b96817789e6ac1d40b7050ae2ac084
abdoulaye <ablaye0m@gmail.com>
2020-10-29 22:08:59 +0100

    ADD util.py

--- loadingdata.py
+++ loadingdata.py
@@ -1,11 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import cv2
-import fix_corrupted_img
-import tqdm
-import random
-import pickle
+from util import *
 
 # Fix corrupted img
 import sys
---
+++ util.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import fix_corrupted_img
+import tqdm
+import random
+import pickle
